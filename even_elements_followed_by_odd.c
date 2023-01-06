45345f041459f2676ff2a81a621ed2cf18ac9c46
karthikeya03 <120096427+karthikeya03@users.noreply.github.com>
2023-01-06 22:59:26 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int A[n],i;     for(i=0;i<n;++i)     {         scanf("%d",&A[i]);     }       for(i=0;i<n;++i)     {         if(A[i]%2==0)         printf("%d ",A[i]);     }
        for(i=0;i<n;++i)
        {
            if(A[i]%2!=0)
            {
                printf("%d ",A[i]);
            }
        }
    }

---
+++ even_elements_followed_by_odd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int A[n],i;
+    for(i=0;i<n;++i)
+    {
+        scanf("%d",&A[i]);
+    }
+      for(i=0;i<n;++i)
+    {
+        if(A[i]%2==0)
+        printf("%d ",A[i]);
+    }
+
+    for(i=0;i<n;++i)
+    {
+        if(A[i]%2!=0)
+        {
+            printf("%d ",A[i]);
+        }
+    }
+}
